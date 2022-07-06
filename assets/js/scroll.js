6377b9c91d69746b2c531c7208c994b89ae94253
Simon Robin <sljrobin@gmail.com>
2022-07-06 21:42:54 +0200

    Updated HTML content

---
+++ assets/js/scroll.js
@@ -0,0 +1,12 @@
+// JavaScript enabling a button that scrolls to the top of a page 
+
+var scrollToTopBtn = document.getElementById("js-btn-scroll-top");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
